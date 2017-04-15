5ec43fee43ca5de52533b1a64bdc5453493dcab9
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-04-16 00:55:27 +0100

    creates user model

---
+++ server/index.js
@@ -0,0 +1,18 @@
+//Main starting point of the server application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+
+// App Setup middleware
+app.use(morgan('combined')); //morgan logs incoming requests, used for debugging
+app.use(bodyParser.json({ type: '*/*'})); //parse incoming requests to json
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
