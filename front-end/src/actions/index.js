53e41afc6325ff64e75d9edeaae5e9ea7ae39ab6
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-04-22 01:51:33 +0100

    builds signup

--- front-end/src/actions/index.js
+++ front-end/src/actions/index.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, {email, password})
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(error => dispatch(authError(error.response.data.error)));
+  }
+}
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
@@ -10,7 +22,6 @@ export function signinUser({ email, password }) {
 
     // Submit email/password to the server
        //below {email, password} is the short for { email:email, password:password }
-       console.log('sending post request');
     axios.post(`${ROOT_URL}/signin`, {email, password})
       .then(response => {
         // If request is good,
@@ -26,6 +37,20 @@ export function signinUser({ email, password }) {
       .catch(() => {
         // If request is bad,
         // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
       });
   }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+
+  return { type: UNAUTH_USER };
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,7 +6,9 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
+import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
+import Signout from './components/auth/signout';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -15,7 +17,9 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <Route path="signup" component={Signup} />
         <Route path="signin" component={Signin} />
+        <Route path="signout" component={Signout} />
       </Route>
     </Router>
   </Provider>
